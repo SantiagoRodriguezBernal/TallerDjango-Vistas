f2543d16b7ba8ad5a1e67a362aaeb6f7bf654682
Zeifyra <s.rodriguez63@uniandes.edu.co>
2023-02-10 22:01:49 -0500

    Edit Settings

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": "ladata2",
+        "USER": "zeifyra",
+        "PASSWORD": "nose",
         "HOST": "localhost",
-        "PORT": "",
+        "PORT": "5432",
     }
 }
 
